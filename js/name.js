aa3c94ab3458c9729608ffd1bd80ac7f1e386bff
Masudur <masudurrahmanmasum251@gmail.com>
2023-07-06 23:10:30 +0600

    complete 80% of these project

--- js/name.js
+++ js/name.js
@@ -2,3 +2,8 @@ setTimeout(() =>{
     document.querySelector(".text_video").style.display="none"; 
     document.querySelector(".heart").style.display="block";
 }, 7000)
+
+const to_left = document.querySelector(".to-top");
+to_left.addEventListener("click", ()=>{
+window.location="cover.html";
+})
